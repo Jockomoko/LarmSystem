dc21d44377a4550687cf692a07ad8bd73643fb8e
RubinAtChas <jonathan.rubin@chasacademy.se>
2024-12-02 16:20:06 +0100

    Removed useless code, updated code

--- Classes/LarmComponents/LarmComponent.h
+++ Classes/LarmComponents/LarmComponent.h
@@ -1,1 +1,13 @@
+class LarmComponent
+{
+    private:
+int component_id;
+int customer_id;
+bool  is_activeComponent;
+bool is_ConnectedComponent;
 
+    public:
+    LarmComponent(int customer_id) : customer_id(customer_id){
+        
+    }
+};
--- Classes/customer.h
+++ Classes/customer.h
@@ -1,13 +1,33 @@
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
+
 #include <string>
+#include <vector>
+#include "User.h"
+#include "LarmComponents/LarmComponent.h"
 
 class Customer
 {
-private:
-std::string companyName;
-int company_id;
 public:
-Customer(std::string companyName, int company_id) : companyName(companyName){
-    
-}
+    Customer(const std::string &name, int id);
 
+    // Getters
+    std::string getName() const;
+    int getId() const;
+    std::vector<User> getUsers() const;
+    std::vector<LarmComponent> getLarmComponents() const;
+
+    // Setters
+    void setName(const std::string &name);
+    void setId(int id);
+    void addUser(const User &user);
+    void addLarmComponent(const LarmComponent &larmComponent);
+
+private:
+    std::string name;
+    int id;
+    std::vector<User> users;
+    std::vector<LarmComponent> larmComponents;
 };
+
+#endif // CUSTOMER_H
