9b3768357fd4bce14036fa63b3f7c07a1ef3f5fb
Fabian Aman <Fabianaman03@gmail.com>
2024-12-02 15:21:31 +0100

    updated class

---
+++ Classes/Users.h
@@ -0,0 +1,12 @@
+#include <iostream>
+
+class User
+{
+private:
+    int user_id;
+    std::string name;
+    std::string password;
+
+public:
+    User(std::string name, std::string password) : name(name), password(password);
+};
